c37d9526a9409a1b5df7cc1aa0dcd38c1f70dbe2
Tom White <tomwhite007@gmail.com>
2022-03-24 07:20:59 +0000

    feat: add shared lib path

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
-import { SharedModule } from 'projects/shared/src';
+import { SharedModule } from '@shared';
 
 @NgModule({
   declarations: [ProductsComponent],
