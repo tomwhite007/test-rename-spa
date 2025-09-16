36ccb6c449ba584236e90cd7006fcd5bcbb3d6e1
Tom White <tomwhite007@gmail.com>
2025-09-16 20:45:01 +0100

    feat: add new cli constructs for tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestGuard } from './test.guard';
+import { TestResolver } from './test.resolver';
 
 const routes: Routes = [
   {
     path: 'products',
     loadChildren: () =>
       import('./products/products.module').then((m) => m.ProductsModule),
+  },
+  {
+    path: 'test',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+    canActivate: [TestGuard],
+    resolve: {
+      test: TestResolver,
+    },
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestInterceptor } from './test.interceptor';
+import { TestPipe } from './test.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TestPipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [TestInterceptor],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
