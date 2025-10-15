e4a8e89d855f87b4c3fa9b4d645a346667592b72
Tom White <tomwhite007@gmail.com>
2025-10-15 20:55:17 +0100

    feat: add UntilDestroy decorator to ProductsComponent

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
